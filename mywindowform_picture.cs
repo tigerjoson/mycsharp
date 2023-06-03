c68b73a39b65281b83a3ef7351c5141b28d3a4bf
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-04 03:46:58 +0800

    Update mywindowform_picture.cs

--- mywindowform_picture.cs
+++ mywindowform_picture.cs
@@ -7,28 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
-    \\classname : class
     {
-    \\in from1.cs use toolbox add picturebox.....
-    \\then declaretion is in Form1.Designer.cs
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void openfile_button_Click(object sender, EventArgs e)
         {
-           
+            OpenFileDialog myopenFileDialog = new OpenFileDialog();
+            if (myopenFileDialog.ShowDialog()== DialogResult.OK) {
+
+                //  pictureBox1.Load(myopenFileDialog.FileName);
+                String picture_position = myopenFileDialog.FileName;
+                //   Image image = pictureBox1.Image;
+                Image<Bgr, Byte> image = new Image<Bgr, byte>(picture_position);
+                Image<Bgr, Byte> image2 = image.CopyBlank();
+                pictureBox1.Image = image.ToBitmap();
+                //  MessageBox.Show(pictureBox1.Image.GetType().ToString()); 
+                int threshold = 70;
+                
+                int Height = pictureBox1.Image.Height;
+                int Width = pictureBox1.Image.Width;
+            
+                image2 = image.ThresholdBinary(new Bgr(threshold, threshold, threshold), new Bgr(255, 255, 255)) ;
+                /*for (int h=0;h<Height ; h++) {
+                    for (int w = 0; w < Width; w++) {
+                    }
+                }*/
+                pictureBox2.Image = image2.ToBitmap();
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
